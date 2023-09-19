34b0ac8361e877da88a4012501a93724b9f48787
tarneo <tarneo@tarneo.fr>
2023-09-19 14:38:27 +0200

    Add responsive font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,20 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-		extend: {
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+			'3xl': '1920px',
+			'4xl': '2560px',
+			'5xl': '3840px',
+			'6xl': '4096px',
+			'7xl': '5120px',
+			'8xl': '6144px',
+			'9xl': '7168px',
+			'10xl': '8192px',
 		}
 	},
   plugins: [],
